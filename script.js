575e26bd661359749fab9fc2aaf29884a97ab2f1
Dasha Borisenko-Orlowski <weissebrauen@yandex.ru>
2020-05-31 20:03:42 -0400

    fix

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 const edit = document.querySelector(".profile__edit-btn");
 const overlay = document.querySelector(".page__overlay");
-const save = document.querySelector(".edit-form__save-btn");
+const form = document.querySelector(".edit-form");
 const close = document.querySelector(".edit-form__close-btn");
 const username = document.querySelector(".profile__username");
 const bio = document.querySelector(".profile__bio");
 const inputUsername = document.querySelector(".edit-form__input_content_username");
 const inputBio = document.querySelector(".edit-form__input_content_bio");
+const pics = document.querySelectorAll(".post__image");
+let images = [
+    './images/yosemite.jpg',
+    './images/lake-louise.png',
+    './images/bald-mountains.png',
+    './images/latemar.png',
+    './images/vanois.png',
+    './images/lago-di-braies.png'
+] 
 
-edit.addEventListener("click", popUp);
-close.addEventListener("click", disappear);
-inputUsername.addEventListener("focus", removePlaceholder);
-inputBio.addEventListener("focus", removePlaceholder);
-inputUsername.addEventListener("focusout", addPlaceholder);
-inputBio.addEventListener("focusout", addPlaceholder);
-save.addEventListener("click", update);
+for (i=0;i<images.length;i++) {
+    pics[i].style.backgroundImage = "url('" + images[i] + "')";
+    console.log(pics[i]);
+}
 
-
+function modal() {
+    overlay.classList.toggle('page__overlay_invisible');
+}
 
 function popUp() {
+    inputUsername.value = "";
+    inputBio.value = "";
     inputUsername.placeholder = username.textContent;
     inputBio.placeholder = bio.textContent;
-    overlay.style.display = "block";
+    modal();
 }
 
-function disappear() {
-    overlay.style.display = "none";
+function update(event) {
+    username.textContent = inputUsername.value;
+    bio.textContent = inputBio.value;
+    modal();
+    event.preventDefault();
 }
 
-function removePlaceholder() {
-    this.placeholder = "";
-}
+edit.addEventListener("click", popUp);
+close.addEventListener("click", modal);
+form.addEventListener("submit", update);
 
-function addPlaceholder() {
-    if (this.value === "") {
-        this.placeholder = "Type something...";
-    }
-    
-}
 
-function update() {
-    if(inputUsername.value !== "") {
-        username.textContent = inputUsername.value;
-    }
-    if(inputBio.value !== "") {
-        bio.textContent = inputBio.value;
-    }
-    disappear();
-}
